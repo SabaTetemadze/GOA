1e34ddf1595007e378b9e0375ffe4b5e9f34542f
saba tetemadze <161717942+SabaTetemadze@users.noreply.github.com>
2025-10-08 17:29:41 +0400

    Add files via upload

---
+++ day 1/class work/la_passion.py
@@ -0,0 +1,15 @@
+print(3+4)
+print(1894+1764)
+print(6+7)
+
+print(11-7)
+print(100-90)
+print(0-0)
+
+print(5*1)
+print(11*11)
+print(12345*0)
+
+print(10000000000000/3)
+print(6767/6969)
+print(10/3)
---
+++ day 1/home work/hw.py
@@ -0,0 +1,21 @@
+"""დეკლარაცია არის ცვლადის შექმნა, ინიციალიზება არის ცვლადისთვის მნიშვნელობის მინიჭება, რეინიციალიზება არის ცვლადის მნიშვნელობის შეცვლა, ალბათ"""
+
+
+name = "saba"
+print(name)
+name = "luka"
+print(name)
+name = "nika"
+print(name)
+
+num1=3
+num2=7
+num3=1
+num4=314159265358979324846
+num5=-10
+print(num1+num2+num3+num4+num5)
+
+width=10
+length=20
+p= width*2+length*2
+print(p)
