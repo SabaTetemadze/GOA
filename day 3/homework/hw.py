2e4281a35694e4de5170360637c724bd3d9cd4a9
saba tetemadze <161717942+SabaTetemadze@users.noreply.github.com>
2025-10-15 20:30:52 +0400

    Add files via upload

---
+++ day 3/homework/hw.py
@@ -0,0 +1,20 @@
+#comment is the way of discribing the script and hat it does for further explanation in case you forgot
+#to create a comment use the '#' sign or 
+
+#case sensitivity means that the programming language sees upper case and lower case letters as different
+#here's an example
+score = 10
+Score = 20
+SCOre = 30
+SCORE = 40
+
+#snake case means using '_' symbol in names of variables instead of spaces
+#exampleeeeeeeeeeees:
+favourite_color = "black"
+favourite_car = "porche 911"
+
+#debugging is running the program and checking if there's any bugs (i think)
+
+print("Hello World")
+city = "London"
+print(city)
