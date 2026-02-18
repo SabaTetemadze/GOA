934587aee7f9860079019ebd9f13ba1d94396792
saba tetemadze <161717942+SabaTetemadze@users.noreply.github.com>
2026-02-18 21:39:49 +0400

    Add files via upload

---
+++ day 37/hw/awe4tg.py
@@ -0,0 +1,11 @@
+#გავიმეორე 👍
+
+
+def greet(name="monkey"):
+  print("hello "+name)
+#name-ს აქვს დეფაულტ პარამეტრი, თუ ფუნქციის გამოწახების დროს name-ს რამე არგუმენტს არ გაუწერ დეფაულტად "monkey"-ს გაუტოლდება gng😭✌
+
+
+def fu(name="tqveni saxeli"):
+  for i in name:
+    print(i)
