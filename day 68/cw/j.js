d8349019ec0b94d7ea29de9eb01f8a3d809b51f5
saba tetemadze <161717942+SabaTetemadze@users.noreply.github.com>
2026-06-08 20:01:37 +0400

    Add files via upload

---
+++ day 68/cw/j.js
@@ -0,0 +1,33 @@
+if (age>=18){
+    console.log("სრულწლოვანი ხარ")
+} else{
+    console.log("არასრულწლოვანი ხარ")
+}
+
+if(number%2===2){
+    console.log("ლუწი")
+} else{
+    console.log("კენტი")
+}
+
+if (score>=90){
+    console.log("A")
+} else if (score>=80){
+    console.log("B")
+} else if (score>=70){
+    console.log("C")
+} else if (score>=60){
+    console.log("D")
+} else if (score<60){
+    console.log("F")
+} 
+
+if (purchaseamunt>=100){
+    let discound = true
+} else{
+    let discount = false
+}
+
+let a = 6
+let b = 7
+a > b ? console.log(a):console.log(b)
